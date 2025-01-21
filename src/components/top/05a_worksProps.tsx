854aa598074b0125c930b1a6982deef669ce149b
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-01-21 15:03:20 +0900

    feat: DB関連のあれこれ追加

--- src/components/top/05a_worksProps.tsx
+++ src/components/top/05a_worksProps.tsx
@@ -1,15 +1,15 @@
 type worksProps = {
     link: string;
-    img: string;
+    imgpath: string;
     title: string;
     description: string;
 }
 
-export default function Works_Card({ link, img, title, description }: worksProps) {
+export default function Works_Card({ link, imgpath, title, description }: worksProps) {
     return (
         <a href={link} target="blank">
             <div className="flex rounded-xl p-1 duration-300 hover:opacity-60 md:p-3">
-                <img src={img} className="size-24 rounded-xl shadow-lg" alt={title} />
+                <img src={imgpath} className="size-24 rounded-xl shadow-lg" alt={title} />
                 <div className="ml-4 flex flex-col gap-3">
                     <h3 className="line-clamp-1 text-xl font-bold md:text-2xl">{title}</h3>
                     <p className="line-clamp-2 text-sm text-primary-800 md:text-base">{description}</p>
---
+++ src/pages/WorksPage.tsx
@@ -0,0 +1,48 @@
+import Works_Card from "../components/top/05a_worksProps";
+import { useEffect, useState } from "react";
+
+type Work = {
+    id: number;
+    link: string;
+    imgpath: string;
+    title: string;
+    description: string;
+};
+
+export default function WorksPage() {
+    const [works, setWorks] = useState<Work[]>([]);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_WORKERS_URL}/works`)
+            .then((response) => response.json())
+            .then((data) => {
+                const sortedData = data
+                    .sort((a: Work, b: Work) => b.id - a.id) // 降順ソート
+                setWorks(sortedData);
+            });
+    }, []);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center bg-primary-300 px-2 py-8 text-primary-800 md:p-8">
+            <div className="relative flex w-full flex-col items-center justify-center rounded-xl bg-[#fff] p-6 shadow-lg">
+                <section className="flex w-full max-w-2xl flex-col gap-4 py-10">
+                    <h1 className="text-center text-3xl font-bold md:text-4xl">-- Works --</h1>
+                    <h1 className="text-center text-base md:text-xl">片手間で作ったものたち</h1>
+                    <section className="flex flex-col gap-6 py-10">
+                        {works.map((work) => (
+                            <Works_Card
+                                key={work.id}
+                                link={work.link}
+                                imgpath={work.imgpath}
+                                title={work.title}
+                                description={work.description}
+                            />
+                        ))}
+                    </section>
+                    <a href="/" className="text-center text-base underline duration-300 hover:opacity-60">トップに戻る</a>
+
+                </section>
+            </div>
+        </main>
+    );
+}
