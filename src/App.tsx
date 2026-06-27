b10d6674a042b74bab4473784e7c9d7c66a8a71a
zawataku <zactaku0819@gmail.com>
2026-06-27 13:25:28 +0900

    feat: ページ遷移

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 import TopPage from './pages/TopPage'
 import WorksPage from './pages/WorksPage'
@@ -6,13 +6,17 @@ import TeapotPage from './pages/TeapotPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
+  const location = useLocation()
+
   return (
-    <Routes>
-      <Route path='/' element={<TopPage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/teapot" element={<TeapotPage />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Routes>
+    <div key={location.pathname} className="page-transition">
+      <Routes>
+        <Route path='/' element={<TopPage />} />
+        <Route path="/works" element={<WorksPage />} />
+        <Route path="/teapot" element={<TeapotPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </div>
   )
 }
 
